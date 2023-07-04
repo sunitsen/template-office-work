24b699271ea0063aec8d6c6031fa54b05803b530
Bushra Nawar <bushra.nawar@sjinnovation.com>
2023-07-04 19:16:29 +0600

    message

--- assets/template/js/template.js
+++ assets/template/js/template.js
@@ -1,21 +1,46 @@
-// Select the location container
-let locationDiv = document.querySelector('.location-div');
-console.log(locationDiv);
+// // Select the location container
+// let locationDiv = document.querySelector('.location-div');
+// console.log(locationDiv);
 
-// Get all the sections within the container
-var sections = locationDiv.querySelectorAll('section');
+// // Get all the sections within the container
+// var sections = locationDiv.querySelectorAll('section');
+
+// // Add a scroll event listener
+// window.addEventListener('scroll', function () {
+//   var visibleSection = getVisibleSection();
+//   if (visibleSection) {
+//     var sectionId = visibleSection.id;
+
+//     var currentURL = window.location.href;
+//     var hashIndex = currentURL.indexOf('#');
+//     var urlWithoutSectionId = currentURL.substring(0, hashIndex !== -1 ? hashIndex : currentURL.length);
+//     var url = urlWithoutSectionId + '#' + sectionId;
+
+//     window.history.replaceState(null, null, url);
+//   }
+// });
+
+// // Function to check which section is currently visible
+// function getVisibleSection() {
+//   for (var i = 0; i < sections.length; i++) {
+//     var section = sections[i];
+//     var rect = section.getBoundingClientRect();
+//     if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+//       return section;
+//     }
+//   }
+//   return null;
+// }
+
+// Get all the sections on the page
+var sections = document.querySelectorAll('section');
 
 // Add a scroll event listener
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function() {
   var visibleSection = getVisibleSection();
   if (visibleSection) {
     var sectionId = visibleSection.id;
-
-    var currentURL = window.location.href;
-    var hashIndex = currentURL.indexOf('#');
-    var urlWithoutSectionId = currentURL.substring(0, hashIndex !== -1 ? hashIndex : currentURL.length);
-    var url = urlWithoutSectionId + '#' + sectionId;
-
+    var url = window.location.href.split('#')[0] + '#' + sectionId;
     window.history.replaceState(null, null, url);
   }
 });
@@ -25,14 +50,12 @@ function getVisibleSection() {
   for (var i = 0; i < sections.length; i++) {
     var section = sections[i];
     var rect = section.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+    if (rect.top <= 0 && rect.bottom >= 0) {
       return section;
     }
   }
   return null;
 }
-
-
 
 
 
